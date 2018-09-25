61d9541c27f9442307428faca262e2499585bceb
Chien Wen Liu <dreamuk@gmail.com>
2018-09-25 15:10:34 +0800

    add auction logic, and deal order when auction

--- src/components/auction.js
+++ src/components/auction.js
@@ -7,82 +7,141 @@ import {dispatchToDatabase} from "../reducer/reducer.js";
 export default class Auction extends React.Component {
   constructor(props) {
     super(props);
-    this.setBid = this.setBid.bind(this);
-    this.validateBid = this.validateBid.bind(this);
+
+    let game = this.props.game;
+    this.state = {
+      currentTrick: game.bid.trick
+    };
+    this.setTrump = this.setTrump.bind(this);
+    this.updateBid = this.updateBid.bind(this);
   }
-  validateBid(bid) {
-    let PASS = 5;
-    let AllTRUMP = -1;
-    let {trick, trump} = this.props.game.bid;
-    if (bid.trick >= 0) {
-      if (bid.trick > trick) {
-        // 0 means bid trick, 1 means trump options
-        let newBid = Object.assign({}, this.props.game.bid, {
-          trick: bid.trick,
-          trump: AllTRUMP
-        });
-        this.setBid(newBid);
-      }
-    } else {
-      if (bid.trump === PASS) {
-        return;
-      }
-      let newBid = Object.assign({}, this.props.game.bid, {
-        trump: bid.trump
+  setTrump(index) {
+    this.setState({currentTrick: index});
+  }
+  updateBid(trump, opt = null) {
+    let newBid;
+    if (trump >= 0) {
+      let bid = {
+        trick: this.state.currentTrick,
+        trump: trump
+      };
+      // udpate result
+      let result = this.props.game.bid.result || [];
+      result.push(Object.assign({}, bid));
+      // update bid
+      newBid = Object.assign({}, this.props.game.bid, bid, {
+        result: result
       });
-      this.setBid(newBid);
+    } else {
+      let result = this.props.game.bid.result || [];
+      result.push({opt: opt});
+      // update bid
+      newBid = Object.assign({}, this.props.game.bid, {
+        result: result
+      });
     }
-  }
-  setBid(bid) {
+
+    let deal = this.props.game.deal;
+    deal = (deal + 1) % 4;
+
+    let newGame = Object.assign(
+      {},
+      this.props.game,
+      {bid: newBid},
+      {deal: deal},
+    );
+
     dispatchToDatabase("UPDATE_AUCTION", {
       id: this.props.tableId,
       gameIndex: this.props.gameIndex,
-      bid: bid
+      game: newGame
     });
   }
   render() {
-    let game = this.props.game;
-    let {trick, trump} = this.props.game.bid;
-    let trickOption = BID_NUM.map((num, index) => {
-      // if in that bid trick, all trump options are all selected,
-      // don't show that trick option
+    let {game, tableId, gameIndex} = this.props;
+    let value = game.bid.trick * 4 + game.bid.trump;
+    // todo: refactor
+    let trickOpt, trumpOpt;
+    if (value < 0) {
+      trickOpt = Array.from({length: 7})
+        .fill(0)
+        .map((opt, index) => index);
+      trumpOpt = Array.from({length: 5})
+        .fill(0)
+        .map((opt, index) => index);
+    } else if (value === 28) {
+      trickOpt = [];
+      trumpOpt = [];
+    } else if (value % 4 === 0 && value !== 0) {
+      trickOpt = Array.from({length: 7})
+        .fill(0)
+        .map((opt, index) => index)
+        .filter(opt => opt > game.bid.trick);
+      trumpOpt = Array.from({length: 5})
+        .fill(0)
+        .map((opt, index) => index);
+    } else {
+      trickOpt = Array.from({length: 7})
+        .fill(0)
+        .map((opt, index) => index)
+        .filter(opt => opt >= game.bid.trick);
+      trumpOpt = Array.from({length: 5})
+        .fill(0)
+        .map((opt, index) => index)
+        .filter(opt => opt > game.bid.trump);
+    }
 
-      if (index > trick || (index === trick && trump < 4)) {
-        return (
-          <button
-            onClick={() => this.validateBid({trick: index})}
-            key={getRandomKey()}>
-            {num}
-          </button>
-        );
-      } else {
-        return null;
-      }
-    });
-    let trumpOption = Object.keys(SUIT_SHAPE).map((opt, index) => {
-      if (index > trump || trump === 4) {
-        return (
-          <button
-            onClick={() =>
-              this.validateBid({
-                trump: index
-              })
-            }
-            key={getRandomKey()}>
-            {SUIT_SHAPE[opt](0.2)}
-          </button>
-        );
-      }
-    });
+    let allTrickOpt = trickOpt.map((opt, index) => (
+      <button onClick={() => this.setTrump(opt)} key={getRandomKey()}>
+        {opt + 1}
+      </button>
+    ));
+
+    let selectedTrump =
+            this.state.currentTrick === trickOpt[0]
+              ? trumpOpt
+              : [0, 1, 2, 3, 4];
+
+    if (value === 28) {
+      selectedTrump = [];
+    }
+    selectedTrump = selectedTrump.map(opt => (
+      <div onClick={() => this.updateBid(opt, null)} key={getRandomKey()}>
+        {SUIT_SHAPE[opt](0.2)}
+      </div>
+    ));
+    let result = game.bid.result;
+
+    let DoubleBtn = result &&
+            !result[result.length - 1].opt && (
+      <button onClick={() => this.updateBid(-1, "Double")}>
+                    Dboule
+      </button>
+    );
+
+    let ReDoubleBtn = result &&
+            result[result.length - 1].opt === "Double" && (
+      <button onClick={() => this.updateBid(-1, "ReDouble")}>
+                    ReDouble
+      </button>
+    );
 
     return (
       <div>
         <h2>auction</h2>
-        <span>
-                    current Bid trick: {game.bid.trick} trump:{game.bid.trump}
-        </span>
-        <div> {trickOption}</div>
-        <div>{trumpOption}</div>
+        <div>
+          <h3>Bid record</h3>
+        </div>
+        <div>
+          <h3> Auction </h3>
+          {DoubleBtn}
+          {ReDoubleBtn}
+          <button onClick={() => this.updateBid(null, "Pass")}>
+                        pass
+          </button>
+          <div>{allTrickOpt}</div>
+          <div>{selectedTrump}</div>
+        </div>
       </div>
     );
   }
