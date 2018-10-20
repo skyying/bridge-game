78c3664d43726bf4876f9c7057ea856ee6014b35
Chien Wen Liu <dreamuk@gmail.com>
2018-10-20 21:14:26 +0800

    fixed bugs in auction phase. Valid bid would be cancelled after 8 pass bid

--- functions/src/auction.js
+++ functions/src/auction.js
@@ -1,39 +1,45 @@
 const Db = require("./db.js");
 const state = require("./gameState.js");
+const AUCTION_TIMES = 8;
+const PLAYER_NUM = 4;
 
 exports.update = table => {
     let updatedTable = Object.assign({}, table);
-
     if (!updatedTable.game.bid.result) {
         updatedTable.game.bid.result = [{opt: "Pass"}];
     } else {
         updatedTable.game.bid.result.push({opt: "Pass"});
     }
-
     let result = updatedTable.game.bid.result;
 
-    // check it overbid
+    // check if last two round of bids are all invalid, if yes, close this table
     if (
-        result.length >= 8 &&
-        result.slice(result.length - 8).every(bid => bid.opt === "Pass")
+        result.length >= AUCTION_TIMES &&
+        result
+            .slice(result.length - AUCTION_TIMES, result.length)
+            .every(bid => bid.opt === "Pass")
     ) {
         updatedTable.gameState = state.phase.close;
     }
 
-    updatedTable.game.deal = (table.game.deal + 1) % 4;
+    updatedTable.game.deal = (table.game.deal + 1) % PLAYER_NUM;
     updatedTable.timeStamp = new Date().getTime();
 
     Db.setTableDataById(updatedTable);
 };
 
 exports.isFinish = function(table) {
-    if (!table || !table.game.bid.result || table.game.bid.result.length < 4) {
+    if (
+        !table ||
+        !table.game.bid.result ||
+        table.game.bid.result.length < PLAYER_NUM
+    ) {
         return false;
     }
     let game = table.game;
     let result = game.bid.result;
     let isAllPass = result
-        .slice(result.length - 3, result.length)
+        .slice(result.length - PLAYER_NUM - 1, result.length)
         .every(res => res.opt === "Pass");
     return isAllPass && game.bid.trump >= 0;
 };
