8fb528577ef1faa718609f2e3974231f1511b254
Chien Wen Liu <dreamuk@gmail.com>
2018-10-30 23:19:00 +0800

    refactor team score class

--- src/logic/teamscore.js
+++ src/logic/teamscore.js
@@ -1,4 +1,4 @@
-import {RESULT} from "../components/constant.js";
+import {RESULT, PLAYER_NUM} from "../components/constant.js";
 export default class TeamScore {
   constructor(table, currentUser) {
     const BASE_TRICK = 6;
@@ -10,15 +10,17 @@ export default class TeamScore {
     this.targetTrick = this.bid.trick + 1 + BASE_TRICK;
     this.isCurrentUserAPlayer = this.playerIndex >= 0;
     this.scoreboard = this.getScore(table.game);
-    this.result = this.getResult();
+    this.result = this.getResult(table.game);
   }
-  getResult() {
+  getResult(game) {
     let isPlayerInDeclarerTeam = this.playerIndex % 2 === this.declarer % 2;
     let isUserWin = false,
       resultWords;
     if (
-      isPlayerInDeclarerTeam &&
-            this.scoreboard.currentPlayer >= this.targetTrick
+      (isPlayerInDeclarerTeam &&
+                this.getScore(game).teamOne >= this.targetTrick) ||
+            (!isPlayerInDeclarerTeam &&
+                this.getScore(game).teamOne > 13 - this.targetTrick)
     ) {
       resultWords = RESULT.win;
       isUserWin = true;
@@ -36,25 +38,36 @@ export default class TeamScore {
     let index = this.players.indexOf(user);
     return index >= 0 ? index : 0;
   }
-  getScore(game) {
-    const MEMBER_COUNT = 2;
-    let currentPlayerScore = 0,
-      opponentScore = 0;
-    game.cards.map(card => {
+  getScoreByCondition(game, condition) {
+    let teamOne = 0,
+      teamTwo = 0;
+    game.cards.map((card, index) => {
       let count = card.isWin ? 1 : 0;
-      if (
-        card.player %
-                MEMBER_COUNT ===
-                this.playerIndex % MEMBER_COUNT
-      ) {
-        currentPlayerScore += count;
+      if (condition(card, index)) {
+        teamOne += count;
       } else {
-        opponentScore += count;
+        teamTwo += count;
       }
     });
     return {
-      currentPlayer: currentPlayerScore,
-      opponent: opponentScore
+      teamOne: teamOne,
+      teamTwo: teamTwo
     };
+  }
+  getDefaultScore(game) {
+    let condition = (card, index) => {
+      const MEMBER_COUNT = 2;
+      return (index % PLAYER_NUM) % MEMBER_COUNT === 0;
+    };
+    return this.getScoreByCondition(game, condition);
+  }
+  getScore(game) {
+    let condition = (card, index) => {
+      const MEMBER_COUNT = 2;
+      return (
+        card.player % MEMBER_COUNT === this.playerIndex % MEMBER_COUNT
+      );
+    };
+    return this.getScoreByCondition(game, condition);
   }
 }
