e503bc7fdbe832f06ba91f6a3e2560f5ee237b1b
Chien Wen Liu <dreamuk@gmail.com>
2018-11-09 15:25:17 +0800

    add files

---
+++ test/layout.test.js
@@ -0,0 +1,17 @@
+// import TrickLogic from "../src/logic/trick.js";
+// import {testData} from "./testData/testData.js";
+
+// describe("TrickLogic, should output correct information of current trick", () => {
+//   let currentTrick = new TrickLogic();
+//   let game = testData.tables["gameover"].game;
+
+//   it("should outupt null if no game, or game.cards", () => {
+//     expect(currentTrick.validateProps(null)).toEqual(null);
+//   });
+//   it("should outupt game if has game", () => {
+//     expect(currentTrick.validateProps(game)).toEqual(game);
+//   });
+//   it("should outupt correct first card of current trick", () => {
+//     expect(currentTrick.getFirstCardOfCurrentTrick(game)).toEqual(null);
+//   });
+// });
