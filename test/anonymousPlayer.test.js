48c9cbaba41a9854b52c86acc4de752ff065ae8e
Chien Wen Liu <dreamuk@gmail.com>
2019-05-05 00:14:58 +0800

    Fix failed test case

--- test/anonymousPlayer.test.js
+++ test/anonymousPlayer.test.js
@@ -1,23 +1,23 @@
-import AnonymousPlayer from "../src/logic/anonymousPlayer.js";
+import AnonymousUser from "../src/logic/anonymousUser.js";
 
 describe("AnonymousPlayer, should create correct anonymous player", () => {
-  let anonymousPlayer = new AnonymousPlayer();
+  let anonymousUser = new AnonymousUser();
 
   it("should have correct uid", () => {
-    expect(anonymousPlayer.uid.length).toBeGreaterThan(0);
+    expect(anonymousUser.uid.length).toBeGreaterThan(0);
   });
 
   it("should have correct displayName", () => {
-    expect(anonymousPlayer.displayName.length).toBeGreaterThan(0);
+    expect(anonymousUser.displayName.length).toBeGreaterThan(0);
   });
 
   it("should have correct displayName in user Info", () => {
-    expect(anonymousPlayer.userInfo.displayName).toBe(
-      anonymousPlayer.displayName
+    expect(anonymousUser.userInfo.displayName).toBe(
+      anonymousUser.displayName
     );
   });
 
   it("should have correct user type", () => {
-    expect(anonymousPlayer.isAnonymousUser).toBeTruthy();
+    expect(anonymousUser.isAnonymousUser).toBeTruthy();
   });
 });
