7490a8cb5765bfd7eade9d6408fb5f2a8d880fe3
skyying <dreamuk@gmail.com>
2020-07-12 10:27:06 +0800

    feat(package): Upgrade packages and using yarn instead of npm

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const path = require("path");
 const glob = require("glob");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const parts = require("./config/webpack.parts.js");
 
 const PATHS = {
