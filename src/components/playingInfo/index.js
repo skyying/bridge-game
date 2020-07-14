ea969a76d35c8348cb374ffb13d5e2a4496ef2d8
skyying <dreamuk@gmail.com>
2020-07-14 22:01:30 +0800

    refactor(playerInfo): replace class component with hooks

--- src/components/playingInfo/index.js
+++ src/components/playingInfo/index.js
@@ -1,47 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
 import {AuctionResult} from "../auction/auctionResult.js";
 import {GAME_STATE} from "../constant";
 import GameResult from "../gameResult";
 
-export default class PlayingInfo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    let {
-      isSidebarPanelShown,
-      currentUser,
-      windowWidth,
-      windowHeight,
-      table,
-      hands,
-      cardsByPlayer,
-      isTrickFinish
-    } = this.props;
+
+export default function PlayingInfo({
+                         isSidebarPanelShown,
+                         currentUser,
+                         windowWidth,
+                         windowHeight,
+                         table,
+                         hands,
+                         cardsByPlayer,
+                         isTrickFinish
+                     }) {
 
     let canSwitchToSmallerPanel =
-            (this.props.isSidebarPanelShown &&
-                this.props.windowWidth <= 1300) ||
-            this.props.windowWidth <= 1000;
+        (isSidebarPanelShown &&
+            windowWidth <= 1300) ||
+        windowWidth <= 1000;
 
     if (table.gameState === GAME_STATE.auction) {
-      return null;
+        return null;
     }
 
     return (
-      <div>
-        <AuctionResult
-          canSwitchToSmallerPanel={canSwitchToSmallerPanel}
-          isSidebarPanelShown={isSidebarPanelShown}
-          currentUser={currentUser}
-          windowWidth={windowWidth}
-          windowHeight={windowHeight}
-          table={table}
-        />
-        <GameResult currentUser={currentUser} table={table} />
-      </div>
+        <div>
+            <AuctionResult
+                canSwitchToSmallerPanel={canSwitchToSmallerPanel}
+                isSidebarPanelShown={isSidebarPanelShown}
+                currentUser={currentUser}
+                windowWidth={windowWidth}
+                windowHeight={windowHeight}
+                table={table}
+            />
+            <GameResult currentUser={currentUser} table={table}/>
+        </div>
     );
-  }
 }
+
+
