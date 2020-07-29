b4bfad5a8ef9fea6f7943352e58bf63479a4ed0c
skyying <dreamuk@gmail.com>
2020-07-29 23:02:58 +0800

    feat: support typescript and writing tests with typescript

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom-fifteen',
     transform: {'^.+\\.ts?$': 'ts-jest'},
-    testEnvironment: 'node',
     testRegex: '.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {"\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js"}
 };
